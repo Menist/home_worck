89add89771cba1c4991bdc8bc2df17512b608f06
Menist <bastianvanhenning@gmail.com>
2020-11-09 21:50:42 +0300

    add files

---
+++ script.js
@@ -0,0 +1,46 @@
+// task A
+// Создадим переменную, которая содержит строку;
+// Выведем значение этой переменной в alert;
+// Изменим значение переменной;
+// Результат снова выведем в консоль;
+
+let string = 'string';
+alert(string);
+string = 'new string';
+console.log(string);
+//
+zcxczxc
+// // task B
+// Создадим шесть переменных, в каждой из которых будет разных типов данных;
+// Выводим их в alert;
+// Выводим объекты в консоль.
+
+// let number = 6;
+// let string1 = 'string';
+// let boolean = true;
+// let undef = undefined;
+// let nul = null;
+// alert(number)
+// alert(string1)
+// alert(boolean)
+// alert(undef)
+// alert(nul)
+// let obj = {
+//   key: 'val',
+//   key1: 'val1',
+//   key2: 'val2'
+// }
+// console.log(obj);
+
+// task C
+// Выводим окно prompt с вопросом о возрасте;
+// В зависимости от ответа выводим alert с текстом “поздравляем, вам n полных лет”;
+// Выводим окно alert с утверждением совершеннолетний пользователь или нет.
+
+// let age = prompt('Сколько Вам лет?');
+// alert(`Поздравляем, вам ${age} полных лет!`);
+// if (age >= 18) {
+//   alert('Вы совершеннолетний');
+// } else {
+//   alert('Вы не совершеннолетний');
+// }
